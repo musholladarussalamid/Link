d2a87b0c2ddf2e4d05925273659f54f0b5f815d4
Muhammad  Muqorrobin <muhammad.muqorrobin@mitracomm.com>
2025-01-04 15:12:58 +0700

    fixing copy function krdopm

--- src/app/Program/Krdopm/page.tsx
+++ src/app/Program/Krdopm/page.tsx
@@ -59,6 +59,33 @@ const Krdopm = () => {
     }
   }, [linkContent]);
 
+  const unsecuredCopyToClipboard = (text: string) => {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand("copy");
+    } catch (err) {
+      console.error("Unable to copy to clipboard", err);
+    }
+    document.body.removeChild(textArea);
+  };
+
+  /**
+   * Copies the text passed as param to the system clipboard
+   * Check if using HTTPS and navigator.clipboard is available
+   * Then uses standard clipboard API, otherwise uses fallback
+   */
+  const copylink = (content: string) => {
+    if (window.isSecureContext && navigator.clipboard) {
+      navigator.clipboard.writeText(content);
+    } else {
+      unsecuredCopyToClipboard(content);
+    }
+  };
+
   return (
     <div className=" h-screen overflow-scroll">
       <SekilasInfo />
